20a13a8b97452197432c78434ec6c2df0d316227
Aryan736 <aryansingh49072@gmail.com>
2026-04-27 00:44:32 +0530

    here stash works

--- hello.js
+++ hello.js
@@ -1,7 +1,13 @@
 console.log("Hello, World!");
 
+// Suppose you are working on a code and face urgent bug which need to be fixed.
 // just by writting git stash you work dissapers and you can work on something else like bug and fixes
 // SO like code code code
 //after fix git add .//git commit -m "bug fixes"
 //after that you can git stash pop
 //to get your code back and continue working on it
+
+//let  say you write code where you were working and about to push you work
+//CODE
+//CODE
+//CODE
