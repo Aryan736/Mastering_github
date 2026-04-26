51931d925dc88445513c95cac8370c30a33c309f
Aryan736 <aryansingh49072@gmail.com>
2026-04-27 00:37:54 +0530

    saved the day

--- hello.js
+++ hello.js
@@ -1,2 +1,7 @@
 console.log("Hello, World!");
 
+// just by writting git stash you work dissapers and you can work on something else like bug and fixes
+// SO like code code code
+//after fix git add .//git commit -m "bug fixes"
+//after that you can git stash pop
+//to get your code back and continue working on it
